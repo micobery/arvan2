26d64b092fb0f5f5bf04078119a6d42164ca67cf
milad <micobery@gmail.com>
2023-03-27 05:08:53 +0430

    Initial commit

---
+++ src/test/java/com/example/arvan2/Arvan2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.arvan2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Arvan2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
